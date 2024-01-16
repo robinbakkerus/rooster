828f61309fc91ba4abe6a4c61ad27859711ce58e
robin.bakkerus <robin.bakkerus@gmail.com>
2024-01-16 08:41:30 +0100

    accesscode

--- lib/page/ask_accesscode_page.dart
+++ lib/page/ask_accesscode_page.dart
@@ -2,6 +2,7 @@ import 'package:rooster/controller/app_controler.dart';
 import 'package:rooster/data/app_data.dart';
 import 'package:rooster/util/app_mixin.dart';
 import 'package:flutter/material.dart';
+import 'package:rooster/widget/send_accesscode_widget.dart';
 
 class AskAccessCodePage extends StatefulWidget {
   const AskAccessCodePage({super.key});
@@ -11,19 +12,20 @@ class AskAccessCodePage extends StatefulWidget {
 }
 
 class _AskAccessCodePageState extends State<AskAccessCodePage> with AppMixin {
-  final _textCtrl = TextEditingController();
+  final _textCtrl1 = TextEditingController();
+
   bool _findTriggered = false;
 
   @override
   void initState() {
     super.initState();
 
-    _textCtrl.addListener(_onTextFieldChanged);
+    _textCtrl1.addListener(_onTextFieldChanged);
   }
 
   @override
   void dispose() {
-    _textCtrl.dispose();
+    _textCtrl1.dispose();
     super.dispose();
   }
 
@@ -50,11 +52,11 @@ class _AskAccessCodePageState extends State<AskAccessCodePage> with AppMixin {
                     SizedBox(
                       width: 200,
                       child: TextField(
-                        controller: _textCtrl,
+                        controller: _textCtrl1,
                         textCapitalization: TextCapitalization.characters,
                         onChanged: (value) {
-                          if (_textCtrl.text != value.toUpperCase()) {
-                            _textCtrl.value = _textCtrl.value
+                          if (_textCtrl1.text != value.toUpperCase()) {
+                            _textCtrl1.value = _textCtrl1.value
                                 .copyWith(text: value.toUpperCase());
                           }
                         },
@@ -64,12 +66,15 @@ class _AskAccessCodePageState extends State<AskAccessCodePage> with AppMixin {
                         ),
                       ),
                     ),
-                    Container(
-                      width: 20,
-                    ),
-                    //
                   ],
                 ),
+                wh.verSpace(10),
+                TextButton(
+                    onPressed: _onSendAccessCode,
+                    child: const Text(
+                      'Accescode vergeten ?',
+                      style: TextStyle(color: Colors.red),
+                    )),
               ],
             ),
           ),
@@ -79,9 +84,9 @@ class _AskAccessCodePageState extends State<AskAccessCodePage> with AppMixin {
   }
 
   void _onTextFieldChanged() async {
-    final text = _textCtrl.text;
+    final text = _textCtrl1.text;
     if (text.length == 4 && !_findTriggered) {
-      String accessCode = _textCtrl.text.toUpperCase();
+      String accessCode = _textCtrl1.text.toUpperCase();
       await _findTrainer(accessCode);
     }
   }
@@ -94,5 +99,31 @@ class _AskAccessCodePageState extends State<AskAccessCodePage> with AppMixin {
       final String msg = 'Kan geen trainer met accesscode $accesscode vinden!';
       wh.showSnackbar(msg, color: Colors.orange);
     }
+  }
+
+  void _onSendAccessCode() {
+    Widget closeButton = TextButton(
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true)
+            .pop(); // dismisses only the dialog and returns nothing
+      },
+      child: const Text("CLose"),
+    ); // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text('Stuur accesscode'),
+      content: const SizedBox(
+        height: 150,
+        child: SendAccessCodeWidget(),
+      ),
+      actions: [
+        closeButton,
+      ],
+    ); // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
---
+++ lib/widget/send_accesscode_widget.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:rooster/data/app_data.dart';
+import 'package:rooster/model/app_models.dart';
+import 'package:rooster/repo/firestore_helper.dart';
+import 'package:rooster/util/app_mixin.dart';
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
+
+class SendAccessCodeWidget extends StatefulWidget {
+  const SendAccessCodeWidget({super.key});
+
+  @override
+  State<SendAccessCodeWidget> createState() => _SendAccessCodeWidgetState();
+}
+
+class _SendAccessCodeWidgetState extends State<SendAccessCodeWidget>
+    with AppMixin {
+  final _textCtrl2 = TextEditingController();
+  String _foundTrainerByFirstname = ''; //used in alertdialog
+  List<Trainer> _allTrainers = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Vul je voornaam in ...'),
+        wh.verSpace(5),
+        TextField(
+          controller: _textCtrl2,
+          textCapitalization: TextCapitalization.characters,
+          onChanged: (value) {
+            _onFirstnameChanged(value);
+          },
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Voornaam',
+            isDense: true, // Added this
+          ),
+        ),
+        Text(_foundTrainerByFirstname),
+      ],
+    );
+  }
+
+  void _onFirstnameChanged(String value) async {
+    if (_allTrainers.isEmpty) {
+      _allTrainers = await FirestoreHelper.instance.getAllTrainers();
+    }
+
+    if (value.isNotEmpty) {
+      String camelcase = value.substring(0, 1).toUpperCase();
+      if (value.length > 1) {
+        camelcase += value.substring(1).toLowerCase();
+      }
+      if (_textCtrl2.text != value.toUpperCase()) {
+        _textCtrl2.value = _textCtrl2.value.copyWith(text: camelcase);
+      }
+    }
+
+    if (value.length > 2) {
+      for (Trainer t in AppData.instance.getAllTrainers()) {
+        lp('${t.firstName()}.');
+      }
+      Trainer? trainer = _allTrainers.firstWhereOrNull(
+          (e) => e.firstName().toLowerCase() == value.toLowerCase());
+      if (trainer != null) {
+        setState(() {
+          _foundTrainerByFirstname =
+              'gevonden, email is verstuurd!\nja kan het scherm sluiten.';
+        });
+        FirestoreHelper.instance.sendEmail(
+            to: [trainer],
+            cc: [],
+            subject: 'AccessCode',
+            html: _buildMailHtml(trainer));
+      } else {
+        setState(() {
+          _foundTrainerByFirstname = 'onbekende voornaam ...';
+        });
+      }
+    }
+  }
+
+  String _buildMailHtml(Trainer trainer) {
+    String result = '<div>';
+    result += 'Hallo ${trainer.firstName()} <br><br>';
+    result += 'Je accesscode = ${trainer.accessCode} <br>';
+    result += 'success!';
+    result += '</div>';
+    return result;
+  }
+}
