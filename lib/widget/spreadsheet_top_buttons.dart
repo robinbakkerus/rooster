315c2b07771bb369299e0be536246e0b4495c49a
robin.bakkerus <robin.bakkerus@gmail.com>
2024-02-07 18:55:43 +0100

    boxdecoration

--- lib/widget/spreadsheet_top_buttons.dart
+++ lib/widget/spreadsheet_top_buttons.dart
@@ -3,38 +3,46 @@ import 'package:rooster/event/app_events.dart';
 import 'package:rooster/model/app_models.dart';
 
 class SpreadsheetTopButtons extends StatelessWidget {
-  final int index;
-  const SpreadsheetTopButtons({super.key, required this.index});
+  final int _index;
+  const SpreadsheetTopButtons({super.key, required int index}) : _index = index;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        TextButton(
-          onPressed: () => _onTopButtonPressed(PageEnum.spreadSheet),
-          style: TextButton.styleFrom(
-            foregroundColor: index == 0 ? Colors.grey : Colors.black,
-          ),
-          child: const Text('Schema'),
-        ),
-        TextButton(
-            onPressed: () => _onTopButtonPressed(PageEnum.progess),
-            style: TextButton.styleFrom(
-              foregroundColor: index == 1 ? Colors.grey : Colors.black,
-            ),
-            child: const Text('Voortgang')),
-        TextButton(
-            onPressed: () => _onTopButtonPressed(PageEnum.availability),
-            style: TextButton.styleFrom(
-              foregroundColor: index == 2 ? Colors.grey : Colors.black,
-            ),
-            child: const Text('Beschikbaarheid')),
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _buildButton('Schema', PageEnum.spreadSheet, 0),
+          _buildButton('Voortgang', PageEnum.progess, 1),
+          _buildButton('Beschikbaarheid', PageEnum.availability, 2),
+        ],
+      ),
     );
   }
 
+  //-------------------------
+  Widget _buildButton(String text, PageEnum page, int index) {
+    Color color = _index == index ? Colors.grey : Colors.black;
+    BoxDecoration? box = _index == index
+        ? const BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.blue, width: 2.0)))
+        : null;
+
+    return Container(
+      decoration: box,
+      child: TextButton(
+        onPressed: () => _onTopButtonPressed(page),
+        style: TextButton.styleFrom(
+          foregroundColor: color,
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+
+  //--------------------------
   void _onTopButtonPressed(PageEnum page) {
     AppEvents.fireShowPage(page);
   }
