e004f8701a050048e86e4785158699e729ad7391
robin.bakkerus <robin.bakkerus@gmail.com>
2026-03-25 18:20:00 +0100

    fix

--- lib/data/app_version.dart
+++ lib/data/app_version.dart
@@ -1,4 +1,6 @@
+
 import 'package:rooster/model/app_models.dart';
 
-RunMode appRunModus = RunMode.acc;
-String appVersion = '24-Mar-2026 buildnr: 66';
+RunMode appRunModus = RunMode.prod;
+String appVersion = '25-Mar-2026 buildnr: 67';
+
